5360d4cf837beeab5d0e5f26c3cdd7d2075c7078
Joel Gerber <joel@grrbrr.ca>
2017-06-11 14:49:56 -0400

    User test list definition as system test code
    Having thought through some of the basic features that this
    application needs to support, I wrote them out as system tests,
    picked the one that is easiest to implement, upgraded it as the only
    test that will automatically run, and marked the rest as skipped.

--- tests/system/test_process_invocations.py
+++ tests/system/test_process_invocations.py
@@ -27,6 +27,24 @@ def python_container():
     container.remove()
 
 
+def test_returns_error_when_certbot_hostname_is_not_passed():
+    try:
+        subprocess.check_output(args=['hostel-huptainer'])
+    except subprocess.CalledProcessError as exception:
+        assert 'CERTBOT_HOSTNAME' in exception.output
+    else:
+        pytest.fail('hostel-huptainer did not raise an error.')
+
+
+@pytest.mark.skip('to be tackled later')
+def test_prints_usage_when_help_argument_passed():
+    stdout, _ = subprocess.check_output(
+        args=['hostel-huptainer', '--help'])
+
+    assert 'hostel-huptainer' in stdout
+
+
+@pytest.mark.skip('to be tackled later')
 def test_restarts_container_with_matching_label(
         python_container):
     python_container.start()
@@ -38,3 +56,18 @@ def test_restarts_container_with_matching_label(
 
     python_container.wait(timeout=2)
     assert 'HUPPED' in python_container.logs()
+
+
+@pytest.mark.skip('to be tackled later')
+def test_does_not_restart_containers_with_mismatched_label(
+        python_container):
+    python_container.start()
+
+    subprocess.check_call(
+        args=['hostel-huptainer'],
+        env=os.environ.update({
+            'CERTBOT_HOSTNAME': 'idontmatch.testdomain.tld'}))
+
+    python_container.wait(timeout=2)
+
+    assert 'HUPPED' not in python_container.logs()
