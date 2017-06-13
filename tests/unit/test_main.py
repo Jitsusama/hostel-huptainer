709aa5faefe7248a40bc5f235eb5cdc046e379f0
Joel Gerber <joel@grrbrr.ca>
2017-06-12 21:55:31 -0400

    Move tests into class grouping
    As I'm preparing to test a new object interaction, I thought it
    would be a good time to organize things better.

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -6,45 +6,44 @@ from hostel_huptainer.errors import InputError
 from hostel_huptainer.__main__ import main
 
 
-@pytest.mark.parametrize('environ', [
-    {'thing': 'one'}, {'thing': 'two'}])
-def test_passes_os_environ_to_environment(mocker, environ):
-    mocker.patch('hostel_huptainer.__main__.sys')
-    mocker.patch('hostel_huptainer.__main__.InputError')
-    stub_environ = mocker.patch(
-        'hostel_huptainer.__main__.os.environ',
-        value=environ)
+class TestEnvironmentInteractions(object):
+    @pytest.mark.parametrize('environ', [
+        {'thing': 'one'}, {'thing': 'two'}])
+    def test_passes_os_environ_to_environment(self, mocker, environ):
+        mocker.patch('hostel_huptainer.__main__.sys')
+        mocker.patch('hostel_huptainer.__main__.InputError')
+        stub_environ = mocker.patch(
+            'hostel_huptainer.__main__.os.environ',
+            value=environ)
 
-    mock_environment = mocker.patch(
-        'hostel_huptainer.__main__.Environment')
+        mock_environment = mocker.patch(
+            'hostel_huptainer.__main__.Environment')
 
-    main()
+        main()
 
-    mock_environment.assert_called_once_with(stub_environ)
+        mock_environment.assert_called_once_with(stub_environ)
 
+    def test_properly_calls_sys_exit_on_input_error(self, mocker):
+        mocker.patch('hostel_huptainer.__main__.Environment',
+                     side_effect=InputError)
 
-def test_properly_calls_sys_exit_on_input_error(mocker):
-    mocker.patch('hostel_huptainer.__main__.Environment',
-                 side_effect=InputError)
+        mock_exit = mocker.patch(
+            'hostel_huptainer.__main__.sys.exit')
 
-    mock_exit = mocker.patch(
-        'hostel_huptainer.__main__.sys.exit')
+        main()
 
-    main()
+        mock_exit.assert_called_once_with(1)
 
-    mock_exit.assert_called_once_with(1)
+    @pytest.mark.parametrize('message', ['Danger!', ''])
+    def test_calls_stdout_with_input_error_message_when_raised(
+            self, mocker, message):
+        mocker.patch('hostel_huptainer.__main__.sys')
+        mocker.patch('hostel_huptainer.__main__.Environment',
+                     side_effect=InputError(message))
 
+        mock_stdout = mocker.patch(
+            'hostel_huptainer.__main__.stdout')
 
-@pytest.mark.parametrize('message', ['Danger!', ''])
-def test_calls_stdout_with_input_error_message_when_raised(
-        mocker, message):
-    mocker.patch('hostel_huptainer.__main__.sys')
-    mocker.patch('hostel_huptainer.__main__.Environment',
-                 side_effect=InputError(message))
+        main()
 
-    mock_stdout = mocker.patch(
-        'hostel_huptainer.__main__.stdout')
-
-    main()
-
-    mock_stdout.assert_called_once_with(message)
+        mock_stdout.assert_called_once_with(message)
